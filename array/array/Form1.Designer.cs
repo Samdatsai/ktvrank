1c703721bfcbe606e44ee5a182ff1f8d5091f201
Samdatsai <11155012@mail.hpsh.tp.edu.tw>
2025-03-11 16:09:22 +0800

    Add files via upload

---
+++ array/array/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace array
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(48, 57);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(345, 354);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(439, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(192, 106);
+            button1.TabIndex = 1;
+            button1.Text = "排名";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(439, 145);
+            button2.Name = "button2";
+            button2.Size = new Size(192, 106);
+            button2.TabIndex = 2;
+            button2.Text = "歌曲排序";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(439, 271);
+            button3.Name = "button3";
+            button3.Size = new Size(192, 106);
+            button3.TabIndex = 3;
+            button3.Text = "查詢";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(531, 398);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(651, 14);
+            button4.Name = "button4";
+            button4.Size = new Size(152, 106);
+            button4.TabIndex = 5;
+            button4.Text = "歌星排序";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(651, 145);
+            button5.Name = "button5";
+            button5.Size = new Size(152, 106);
+            button5.TabIndex = 6;
+            button5.Text = "清除";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(textBox2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox2;
+        private Button button4;
+        private Button button5;
+    }
+}
